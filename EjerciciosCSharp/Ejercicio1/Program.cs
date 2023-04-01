2b0eda1f448bc325a70fc3bf321579808a893d04
Julian <julian.schmidt@axiomait.com>
2023-04-01 15:02:13 -0300

    ejercicio 1

--- EjerciciosCSharp/Ejercicio1/Program.cs
+++ EjerciciosCSharp/Ejercicio1/Program.cs
@@ -4,7 +4,67 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int minimo = 0;
+            int maximo = 0;
+            int total = 0;
+
+            PedirValoresYCalcularlos(ref minimo, ref maximo, ref total);
+            Console.WriteLine("Minimo: {0}, Maximo: {1}, Promedio: {2}", minimo, maximo, CalcularPromedio(total, 10));
+
+        }
+
+        public static void PedirValoresYCalcularlos(ref int minimo, ref int maximo, ref int total)
+        {
+            int valorIngresado;
+
+            for (int i = 0; i < 10; i++)
+            {
+                valorIngresado = PedirNumeroEnteroValidado();
+                total += valorIngresado;
+                if (i == 0)
+                {
+                    minimo = valorIngresado;
+                    maximo = valorIngresado;
+                }
+
+                if (minimo > valorIngresado)
+                {
+                    minimo = valorIngresado;
+                }
+
+                if (maximo < valorIngresado)
+                {
+                    maximo = valorIngresado;
+                }
+
+            }
+        }
+
+        public static int CalcularPromedio(int total, int cantidad)
+        {
+            return total / cantidad;
+        }
+
+        public static int PedirNumeroEnteroValidado()
+        {
+            int valorParseado;
+            bool numeroEsValido;
+
+            do
+            {
+                Console.WriteLine("Ingrese el valor: ");
+                numeroEsValido = int.TryParse(Console.ReadLine(), out valorParseado);
+            } while (!numeroEsValido || !Validador.Validar(valorParseado, -100, 100));
+
+            return valorParseado;
+        }
+
+        class Validador
+        {
+            public static bool Validar(int valor, int min, int max)
+            {
+                return valor >= min && valor <= max;
+            }
         }
     }
 }
