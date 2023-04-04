42feeae350689e5a669a311128510e831ac0bea9
Julian <julian.schmidt@axiomait.com>
2023-04-04 00:47:49 -0300

    ejercicio 1 oop

---
+++ EjerciciosCSharp/OOPEjercicio1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OOPEjercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 150000;
+            string? razonSocial = "Julian Schmidt";
+
+            Cuenta miCuenta = new Cuenta(razonSocial, saldo);
+            Console.WriteLine(miCuenta.mostrar());
+
+            saldo = PedirNumeroDecimal("Ingresa el monto a retirar: ");
+            miCuenta.retirar(saldo);
+            Console.WriteLine(miCuenta.mostrar());
+
+            saldo = PedirNumeroDecimal("Monto a ingresar: ");
+            miCuenta.ingresar(saldo);
+            Console.WriteLine(miCuenta.mostrar());
+
+        }
+
+        public static double PedirNumeroDecimal(string mensaje)
+        {
+            double valorParseado;
+            bool numeroEsValido;
+
+            do
+            {
+                Console.Write(mensaje);
+                numeroEsValido = double.TryParse(Console.ReadLine(), out valorParseado);
+            } while (!numeroEsValido);
+
+            return valorParseado;
+        }
+
+        class Cuenta
+        {
+            private string titular;
+            private double cantidad;
+
+            public Cuenta(string? titular, double cantidad)
+            {
+                this.titular = titular;
+                this.cantidad = cantidad;
+            }
+
+            public string GetTituar()
+            {
+                return titular;
+            }
+
+            public double GetCantidad()
+            {
+                return cantidad;
+            }
+
+            public string mostrar()
+            {
+                return $"Nombre de titular: {titular}\nSaldo actual: {cantidad}";
+            }
+
+            public bool ingresar(double monto)
+            {
+                if( monto > 0 )
+                {
+                    cantidad += monto;
+                }
+
+                return monto > 0;
+            }
+
+            public void retirar(double monto)
+            {
+                cantidad -= monto;
+            }
+        }
+    }
+}
